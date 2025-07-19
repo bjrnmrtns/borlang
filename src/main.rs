b3b46991a88a1d2829eca511680b2264585222e6
Bjorn Martens <borjn@proton.me>
2025-07-19 20:53:27 +0200

    add support for numbers

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,31 @@ pub enum TokenizationError {
     NoStringDelimiterFound,
     UnrecognizedToken,
 }
-pub struct Scanner {}
+pub struct Scanner<'s> {
+    src: &'s str,
+    iter: std::iter::Peekable<std::str::CharIndices<'s>>,
+    line: usize,
+}
 
-impl Scanner {
-    pub fn scan<'s>(src: &'s str) -> Result<Vec<Token<'s>>, TokenizationError> {
-        let mut line = 0;
-        let mut iter = src.char_indices().peekable();
+impl<'s> Scanner<'s> {
+    pub fn new(src: &'s str) -> Self {
+        Self {
+            src,
+            iter: src.char_indices().peekable(),
+            line: 0,
+        }
+    }
+    pub fn scan(&mut self) -> Result<Vec<Token<'s>>, TokenizationError> {
+        self.line = 0;
         let mut tokens = Vec::new();
-        while let Some((index, c)) = iter.next() {
+        while let Some((index, c)) = self.iter.next() {
             match c {
                 ' ' => (),
                 '\r' => (),
                 '\t' => (),
-                '\n' => line += 1,
+                '\n' => self.line += 1,
                 _ => {
-                    let token = Self::scan_token(src, &mut iter, &mut line, index, c)?;
+                    let token = self.scan_token(index, c)?;
                     tokens.push(token);
                 }
             }
@@ -25,60 +35,70 @@ impl Scanner {
         return Ok(tokens);
     }
 
-    pub fn scan_token<'s>(
-        src: &'s str,
-        iter: &mut std::iter::Peekable<std::str::CharIndices<'s>>,
-        line: &mut usize,
-        start: usize,
-        c: char,
-    ) -> Result<Token<'s>, TokenizationError> {
+    pub fn scan_token(&mut self, start: usize, c: char) -> Result<Token<'s>, TokenizationError> {
         match c {
-            '(' => Ok(Token::LeftParen { line: *line }),
-            ')' => Ok(Token::RightParen { line: *line }),
+            '(' => Ok(Token::LeftParen { line: self.line }),
+            ')' => Ok(Token::RightParen { line: self.line }),
             '!' => {
-                let next_char = iter.peek();
+                let next_char = self.iter.peek();
                 if Self::next_char_is(next_char, '=') {
-                    iter.next();
-                    Ok(Token::BangEqual { line: *line })
+                    self.iter.next();
+                    Ok(Token::BangEqual { line: self.line })
                 } else {
-                    Ok(Token::Bang { line: *line })
+                    Ok(Token::Bang { line: self.line })
                 }
             }
             '"' => {
-                while let Some((current, c)) = iter.next() {
+                while let Some((current, c)) = self.iter.next() {
                     if c == '\n' {
-                        *line += 1;
+                        self.line += 1;
                     }
                     if c == '"' {
                         return Ok(Token::String {
-                            line: *line,
+                            line: self.line,
                             // do not include the double quotes themselves so schrink by 1 on both sides
-                            lexeme: &src[start..current + 1],
+                            lexeme: &self.src[start..current + 1],
                         });
                     }
                 }
                 Err(TokenizationError::NoStringDelimiterFound)
             }
             '/' => {
-                let next_char = iter.peek();
+                let next_char = self.iter.peek();
                 if Self::next_char_is(next_char, '/') {
-                    let line_comment = *line;
-                    while let Some((index, c)) = iter.next() {
+                    let line_comment = self.line;
+                    while let Some((index, c)) = self.iter.next() {
                         if c == '\n' {
-                            *line += 1;
+                            self.line += 1;
                             return Ok(Token::Comment {
                                 line: line_comment,
-                                lexeme: &src[start..index + 1],
+                                lexeme: &self.src[start..index + 1],
                             });
                         }
                     }
                     return Ok(Token::Comment {
                         line: line_comment,
-                        lexeme: &src[start..],
+                        lexeme: &self.src[start..],
                     });
                 } else {
                     Ok(Token::Slash)
                 }
+            }
+            c if Self::is_digit(c) => {
+                while let Some((index, c)) = self.iter.peek() {
+                    if !Self::is_digit(*c) {
+                        return Ok(Token::Number {
+                            line: self.line,
+                            lexeme: &self.src[start..*index],
+                        });
+                    } else {
+                        self.iter.next();
+                    }
+                }
+                return Ok(Token::Number {
+                    line: self.line,
+                    lexeme: &self.src[start..],
+                });
             }
             _ => Err(TokenizationError::UnrecognizedToken),
         }
@@ -90,6 +110,10 @@ impl Scanner {
         } else {
             return false;
         }
+    }
+
+    pub fn is_digit(c: char) -> bool {
+        c >= '0' && c <= '9'
     }
 }
 
@@ -125,7 +149,7 @@ pub enum Token<'s> {
     // Literals
     Identifier,
     String { line: usize, lexeme: &'s str },
-    Number,
+    Number { line: usize, lexeme: &'s str },
 
     // Keywords
     And,
@@ -167,11 +191,13 @@ mod tests {
                 ! // hello this is another comment
                 !=
                 /
+                312455()
                 " multi-line text is 
                 also ok"
             )
         //this is the last comment of the file"#;
-        let tokens = Scanner::scan(&src)?;
+        let mut scanner = Scanner::new(&src);
+        let tokens = scanner.scan()?;
         print_tokens(tokens.as_slice());
         Ok(())
     }
